4b8c0cf6a7302496dba1ad70fd2484a3a7309fbb
Alwaleed Akhtar <alwaleedakhtar@Alwaleeds-MacBook-Pro.local>
2025-05-23 10:50:41 +0300

    Updated dependencies and startup script

--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Starting deployment..."
 pip install --upgrade pip
+echo "Installing requirements..."
 pip install -r requirements.txt
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app 
+echo "Starting Gunicorn..."
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT 
